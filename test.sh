e2f7adb9a594460af833097e70569c16756bf0e9
bigcat88 <bigcat88@icloud.com>
2025-02-06 14:35:29 +0200

    partially implemented "routes" check from AppAPI Proxy

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
-docker container remove --force harp-prod
+#!/bin/bash
+set -e
+
+docker container remove --force harp-prod || true
 
 docker build -t harp-prod .
 
@@ -6,8 +9,6 @@ docker run --rm \
   -p 8780:8780 \
   -p 8781:8781 \
   -p 8782:8782 \
-  -p 8783:8783 \
-  -p 8784:8784 \
   -e HP_EXAPPS_ADDRESS="0.0.0.0:8780" \
   -e HP_EXAPPS_HTTPS_ADDRESS="0.0.0.0:8781" \
   -e HP_FRP_ADDRESS="0.0.0.0:8782" \
